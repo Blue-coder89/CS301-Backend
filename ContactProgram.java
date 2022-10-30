b9e5e4640ba99e3c6125f0f1e83220bace876588
Blue-coder89 <nish95.sha@gmail.com>
2022-10-30 23:58:13 +0530

    Updated README

--- ContactProgram.java
+++ ContactProgram.java
@@ -3,7 +3,7 @@ public class ContactProgram {
     public static void main(String[] args){
         String jdbcURL = "jdbc:postgresql://localhost:5432/railways"; // jdbc url for postgres database
         String username = "postgres";
-        String password = "Pekka#73";
+        String password = "";
         try{
             Class.forName("org.postgresql.Driver");
             Connection connection  = DriverManager.getConnection(jdbcURL,username,password);
@@ -11,7 +11,6 @@ public class ContactProgram {
             String sql = "SELECT book_tickets(2 , 'A' , 13111 , '2022-10-29' , 'true' ,'A', 'S' );";
             Statement statement = connection.createStatement();
             ResultSet r = statement.executeQuery(sql);
-        
             connection.close();
         } catch (Exception e) {
             e.printStackTrace();
