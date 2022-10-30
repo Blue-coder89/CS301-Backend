b11572994ff3cfac831a2d2bb9a23ab140a947da
Blue-coder89 <nish95.sha@gmail.com>
2022-10-30 23:55:27 +0530

    Installed postgres driver and made a successful connection between java appliaction and postgres.

--- ContactProgram.java
+++ ContactProgram.java
@@ -1,21 +1,22 @@
 import java.sql.*;
-
 public class ContactProgram {
     public static void main(String[] args){
         String jdbcURL = "jdbc:postgresql://localhost:5432/railways"; // jdbc url for postgres database
         String username = "postgres";
-        String password = "";
+        String password = "Pekka#73";
         try{
+            Class.forName("org.postgresql.Driver");
             Connection connection  = DriverManager.getConnection(jdbcURL,username,password);
             System.out.println("Connected to PostgresSQL server");
-            String sql = "SELECT book_tickets(2 , 'A' , 13111 , '2022-10-29' , 'true' ,'Arshdeep', 'Shahnawaz' );";
+            String sql = "SELECT book_tickets(2 , 'A' , 13111 , '2022-10-29' , 'true' ,'A', 'S' );";
             Statement statement = connection.createStatement();
             ResultSet r = statement.executeQuery(sql);
         
             connection.close();
-        } catch (SQLException e) {
-            System.out.println("Error in connecting to postgressql server");
+        } catch (Exception e) {
             e.printStackTrace();
+            System.err.println("Error in connecting to postgressql server");
+            System.exit(0);
         }
     }
 }
